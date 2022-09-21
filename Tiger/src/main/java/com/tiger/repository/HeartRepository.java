ee6a9c6ebb59202f727fbfcce3f7ab834cf65bc2
sungkong <=tjddnths0223@naver.com>
2022-09-21 17:17:22 +0900

    <feat> 좋아 기능 및 위시리스트 추가

---
+++ Tiger/src/main/java/com/tiger/repository/HeartRepository.java
@@ -0,0 +1,20 @@
+package com.tiger.repository;
+
+import com.tiger.domain.member.Member;
+import com.tiger.domain.vehicle.Vehicle;
+import com.tiger.domain.vehicle.heart.Heart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface HeartRepository extends JpaRepository<Heart, Long> {
+
+    Optional<Heart> findByMemberAndVehicle(Member member, Vehicle vehicle);
+    Optional<Heart> findByMemberAndVehicleId(Member member, Long vehicleId);
+    List<Heart> findAllByVehicle(Vehicle vehicle);
+    List<Heart> findAllByMember(Member member);
+
+
+}
+
