bb03b949060e5e0dcb090e763fe713a69a53867e
sungkong <=tjddnths0223@naver.com>
2022-09-30 16:28:19 +0900

    <chore> 예약하기 에러 테스트

--- src/main/java/com/tiger/exception/GlobalException.java
+++ src/main/java/com/tiger/exception/GlobalException.java
@@ -1,24 +1,19 @@
 package com.tiger.exception;
 
-import com.tiger.domain.CommonResponseDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import static com.tiger.exception.StatusCode.*;
 
 @RestControllerAdvice
 public class GlobalException {
 
     @ExceptionHandler(CustomException.class)
-    protected CommonResponseDto<?> handleCustomException(CustomException e) {
-        return CommonResponseDto.fail(e.getStatusCode());
+    protected ResponseEntity<ErrorResponseEntity> handleCustomException(CustomException e) {
+        return ErrorResponseEntity.toResponseEntity(e.getStatusCode());
     }
 
-    @ExceptionHandler(NumberFormatException.class)
-    public CommonResponseDto<?> handleNumberFormatException(){
-        return CommonResponseDto.fail(NUMBER_FORMAT_EXCEPTION);
-    }
-
+//    @ExceptionHandler(NumberFormatException.class)
+//    public ResponseDto<?> handleNumberFormatException(){
+//        return ResponseDto.fail("NumberFormatException","잘못된 요청입니다. ");
+//    }
 }
