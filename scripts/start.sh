552b06716f8fca7074a65ebd0f6136c6c272d367
tanibourne <98215154+tanibourne@users.noreply.github.com>
2022-10-04 01:03:39 +0900

    <chore> profile 경로 수정 (#97)

--- scripts/start.sh
+++ scripts/start.sh
@@ -38,6 +38,6 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 #nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 nohup java -Duser.timezone=Asia/Seoul -jar \
-        -Dspring.config.location=classpath:/application.yml,/home/ubuntu/app/step/zip/application-$IDLE_PROFILE.yml \
+        -Dspring.config.location=classpath:/application.yml,/home/ubuntu/app/step/application-$IDLE_PROFILE.yml \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
